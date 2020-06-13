a80359b76b03ab70fb113fe8a303f5cde9625f74
Suliman <Sulimanaltaleb@gmail.com>
2020-06-13 03:46:59 +0300

    Write down the unittests and README
    Write down the unittests and README
    
    completed unittests for all functionalities in the application
    
    Completed a README documentation Reference

--- README.md
+++ README.md
@@ -92,10 +92,219 @@ To activate virtual server, I run this command
 
 
  1. @TODO: Create an endpoint to handle GET requests for all available categories.
+ ```
+{
+  "categories": {
+    "1": "Science", 
+    "2": "Art", 
+    "3": "Geography", 
+    "4": "History", 
+    "5": "Entertainment", 
+    "6": "Sports"
+  }, 
+  "success": true
+}
+```
  2. @TODO: Create an endpoint to handle GET requests for questions, including pagination (every 10 questions).
+ ```
+ {
+  "categories": {
+    "1": "Science", 
+    "2": "Art", 
+    "3": "Geography", 
+    "4": "History", 
+    "5": "Entertainment", 
+    "6": "Sports"
+  }, 
+  "questions": [
+    {
+      "answer": "Maya Angelou", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 5, 
+      "question": "Whose autobiography is entitled 'I Know Why the Caged Bird Sings'?"
+    }, 
+    {
+      "answer": "Edward Scissorhands", 
+      "category": 5, 
+      "difficulty": 3, 
+      "id": 6, 
+      "question": "What was the title of the 1990 fantasy directed by Tim Burton about a young man with multi-bladed appendages?"
+    }, 
+    {
+      "answer": "Muhammad Ali", 
+      "category": 4, 
+      "difficulty": 1, 
+      "id": 9, 
+      "question": "What boxer's original name is Cassius Clay?"
+    }, 
+    {
+      "answer": "Brazil", 
+      "category": 6, 
+      "difficulty": 3, 
+      "id": 10, 
+      "question": "Which is the only team to play in every soccer World Cup tournament?"
+    }, 
+    {
+      "answer": "Uruguay", 
+      "category": 6, 
+      "difficulty": 4, 
+      "id": 11, 
+      "question": "Which country won the first ever soccer World Cup in 1930?"
+    }, 
+    {
+      "answer": "George Washington Carver", 
+      "category": 4, 
+      "difficulty": 2, 
+      "id": 12, 
+      "question": "Who invented Peanut Butter?"
+    }, 
+    {
+      "answer": "Lake Victoria", 
+      "category": 3, 
+      "difficulty": 2, 
+      "id": 13, 
+      "question": "What is the largest lake in Africa?"
+    }, 
+    {
+      "answer": "The Palace of Versailles", 
+      "category": 3, 
+      "difficulty": 3, 
+      "id": 14, 
+      "question": "In which royal palace would you find the Hall of Mirrors?"
+    }, 
+    {
+      "answer": "Agra", 
+      "category": 3, 
+      "difficulty": 2, 
+      "id": 15, 
+      "question": "The Taj Mahal is located in which Indian city?"
+    }, 
+    {
+      "answer": "Escher", 
+      "category": 2, 
+      "difficulty": 1, 
+      "id": 16, 
+      "question": "Which Dutch graphic artist\u2013initials M C was a creator of optical illusions?"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 20
+}
+```
  3. @TODO: Create an endpoint to DELETE question using a question ID.
+ ```
+{
+    'success': True,
+    'deleted_question':question_id
+}
+```
  4. @TODO: Create an endpoint to POST a new question.
+ ```
+{
+    'success': True,
+    'total_questions':len(selection)
+}
+```
  5. @TODO: Create a POST endpoint to get questions based on a search term.
+ ```
+{
+  "questions": [
+    {
+      "answer": "Muhammad Ali", 
+      "category": 4, 
+      "difficulty": 1, 
+      "id": 9, 
+      "question": "What boxer's original name is Cassius Clay?"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 1
+}
+
+```
  6. @TODO: Create a GET endpoint to get questions based on category.
+
+```
+ {
+  "category": 1, 
+  "questions": [
+    {
+      "answer": "The Liver", 
+      "category": 1, 
+      "difficulty": 4, 
+      "id": 20, 
+      "question": "What is the heaviest organ in the human body?"
+    }, 
+    {
+      "answer": "Alexander Fleming", 
+      "category": 1, 
+      "difficulty": 3, 
+      "id": 21, 
+      "question": "Who discovered penicillin?"
+    }, 
+    {
+      "answer": "Blood", 
+      "category": 1, 
+      "difficulty": 4, 
+      "id": 22, 
+      "question": "Hematology is a branch of medicine involving the study of what?"
+    }, 
+    {
+      "answer": "Suliman Altaleb", 
+      "category": 1, 
+      "difficulty": 1, 
+      "id": 24, 
+      "question": "who is Suliman altaleb?"
+    }, 
+    {
+      "answer": "Python", 
+      "category": 1, 
+      "difficulty": 5, 
+      "id": 26, 
+      "question": "This Coding program"
+    }
+  ], 
+  "success": true, 
+  "total_questions": 5
+}
+```
  7. @TODO: Create a POST endpoint to get questions to play the quiz.
+ ```
+{
+  "question": 
+    {
+      "answer": "Python", 
+      "category": 1, 
+      "difficulty": 5, 
+      "id": 26, 
+      "question": "This Coding program"
+    }, 
+  "success": true
+}
+```
  8. @TODO: Create error handlers for all expected errors including 404 and 422.
+ > http://127.0.0.1:5000/questions?page=599
+ ```
+ {
+  "error": 404, 
+  "message": "resource not found", 
+  "success": false
+}
+```
+
+## Testing
+To run the tests, on the Terminal "backend" directory, RUN:
+>     $ dropdb trivia_test
+>     $ createdb trivia_test -O suliman
+>     $ psql trivia_test < trivia.psql
+>     python test_flaskr.py
+```
+macbookpro@MacBooks-MacBook-Pro backend % python test_flaskr.py
+..............
+----------------------------------------------------------------------
+Ran 14 tests in 0.497s
+
+OK
+```
+
--- starter/backend/test_flaskr.py
+++ starter/backend/test_flaskr.py
@@ -15,8 +15,20 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format('suliman@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+        self.new_question = {
+            'question': 'flask is a framework of which language?',
+            'answer': 'Python',
+            'difficulty': 5,
+            'category': 1
+        }
+        self.new_question_error = {
+            'question': 'flask is a framework of which language?',
+            'difficulty': 5,
+            'category': 1
+        }
+
 
         # binds the app to the current context
         with self.app.app_context():
@@ -33,6 +45,133 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_all_categories(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['categories']))
+
+    def test_get_pagination(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        self.assertTrue(len(data['categories']))
+    
+    def test_out_of_range_page(self):
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+    
+    def test_search_with_results(self):
+        res = self.client().post('/questions/searchresult', json={'searchTerm': 'flask'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions']>0,True)
+        self.assertIsNotNone((data['questions']))
+    
+    def test_search_without_results(self):
+        res = self.client().post('/questions/searchresult', json={'searchTerm': 'sulimanaltalebUdacityFSND'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions']==0,True)
+        self.assertIsNotNone((data['questions']))
+
+    def test_search_error(self):
+
+        res = self.client().post('/questions/searchresults', json={'searchTerm': 'flask',})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "resource not found")
+
+    def test_category_questions(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone((data['questions']))
+        self.assertTrue((data['total_questions'] > 0 , True))
+        self.assertTrue((data['category'] == 3,True))
+
+    def test_category_questions_error(self):
+        res = self.client().get('/categories/random/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    def test_quiz(self):
+        new_quiz = {'quiz_category': {'type': 'Science', 'id': 1},'previous_questions': []}
+
+        res = self.client().post('/quizzes', json=new_quiz)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_quiz_error(self):
+        new_quiz = {'quiz_category': {'type': 'Art', 'id': 2}}
+        res = self.client().post('/quizzes', json=new_quiz)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")
+
+    def test_delete_question(self):
+        newQuestion = Question(question='is this going to be deleted?', answer='Yes, of course',difficulty=5 , category=1)
+        newQuestion.insert()
+        res = self.client().delete("/questions/{}".format(newQuestion.id))
+        data = json.loads(res.data)
+
+        confirmed_deletion = Question.query.filter(Question.id == newQuestion.id).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted_question'], (newQuestion.id))
+        self.assertEqual(confirmed_deletion, None)
+    
+
+    def test_404_if_question_does_not_exist(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+    
+    def test_create_new_question(self):
+        res = self.client().post('/questions', json=self.new_question)
+        data = json.loads(res.data)
+ 
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['total_questions'] > 0,True)
+        
+    
+    def test_422_if_question_creation_fails(self):
+        res = self.client().post('/questions', json=self.new_question_error)
+        data = json.loads(res.data)
+       
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
 
 
 # Make the tests conveniently executable
